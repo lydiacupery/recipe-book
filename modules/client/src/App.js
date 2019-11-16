c9e67f7ab0e36ef82b594602ac68ad04fa8a8ba7
Lydia <lydia.cupery@atomicobject.com>
2019-11-16 11:22:15 -0500

    adding authentication

--- modules/client/src/App.js
+++ modules/client/src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+//Amplify
+import Amplify, { Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import { createBrowserHistory } from 'history';
+
+import {ApolloProvider} from "react-apollo"
+
+import {buildGraphqlClient} from "./graphql/client"
+
+import AllRecipes from "./AllRecipes"
+
+import {awsconfig} from './aws-exports';
+
+Amplify.configure({
+  Auth: {
+    region: awsconfig.appsync_region,
+    userPoolId: awsconfig.user_pool_id,
+    userPoolWebClientId: awsconfig.user_pool_client_id 
+  }
+})
+
+const history = createBrowserHistory();
+const graphqlClient = buildGraphqlClient({history})
+
+function BaseApp() {
   return (
     <div className="App">
       <header className="App-header">
@@ -11,9 +35,18 @@ function App() {
           <p>
           There will be more content here soon.
           </p>
+      <AllRecipes/>
       </header>
     </div>
   );
 }
 
-export default App;
+const App = withAuthenticator(BaseApp, true)
+
+const EnhancedApp = () => (
+  <ApolloProvider client={graphqlClient}>
+    <App/>
+  </ApolloProvider>
+)
+
+export default EnhancedApp;
